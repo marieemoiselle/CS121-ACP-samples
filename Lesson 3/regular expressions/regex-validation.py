b81c2367c8d9fa6ef4979b77890e83363f821ad0
marieemoiselle <agdonmarie@gmail.com>
2024-07-27 12:11:03 +0800

    changes in regex example

--- Lesson 3/regular expressions/regex-validation.py
+++ Lesson 3/regular expressions/regex-validation.py
@@ -6,4 +6,4 @@ pattern = r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
 is_valid = re.match(pattern, email)
 print(is_valid)  
 
-# Output: <re.Match object; span=(0, 19), match='fatimamarie@gmail.com'>
+# Output: <re.Match object; span=(0, 21), match='fatimamarie@gmail.com'>
