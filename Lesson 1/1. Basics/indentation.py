e6c3eb67661967c1597d16ccbc61287b20d4edc8
marieemoiselle <agdonmarie@gmail.com>
2025-01-03 22:33:19 +0800

    added sample for indentation

---
+++ Lesson 1/1. Basics/indentation.py
@@ -0,0 +1,13 @@
+if 10 < 12:
+    print("This statement is true!")
+    print("This is an indentation.")
+
+# Output:
+# This statement is true!
+# This is an indentation.
+
+# If there is no indentation, Python will throw an error.
+#if 10 < 12:
+#print("This statement is true!")
+
+# IndentationError: expected an indented block after 'if' statement on line 10
